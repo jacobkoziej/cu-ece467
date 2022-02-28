579bf67fcee642f5ca91c4f3c00f68bc229f6a6d
Jacob Koziej <jacobkoziej@gmail.com>
2022-02-27 19:02:33 -0500

    Parse arguments

--- p1/p1.py
+++ p1/p1.py
@@ -16,8 +16,63 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import argparse
+
+
+def arg_gen():
+    parser = argparse.ArgumentParser(description='text categorization')
+    subparser = parser.add_subparsers(
+        help='{test,train} -h/--help',
+        metavar='mode',
+        required=True,
+    )
+
+    test_subparser = subparser.add_parser('test')
+    test_subparser.add_argument(
+        '-d',
+        help='input trained database',
+        metavar='db',
+        required=True,
+        type=argparse.FileType('r'),
+    )
+    test_subparser.add_argument(
+        '-i',
+        help='input test documents',
+        metavar='input',
+        required=True,
+        type=argparse.FileType('r'),
+    )
+    test_subparser.add_argument(
+        '-o',
+        help='output labeled documents',
+        metavar='output',
+        required=True,
+        type=argparse.FileType('w'),
+    )
+
+    train_subparser = subparser.add_parser('train')
+    train_subparser.add_argument(
+        '-i',
+        help='input training documents',
+        metavar='input',
+        required=True,
+        type=argparse.FileType('r'),
+    )
+    train_subparser.add_argument(
+        '-o',
+        help='output trained database',
+        metavar='db',
+        required=True,
+        type=argparse.FileType('w'),
+    )
+
+    return parser
+
+
 def main():
-    return
+    args = arg_gen()
+    args.parse_args()
+
 
 if __name__ == '__main__':
     main()
