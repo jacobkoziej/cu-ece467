3f5cff038a7e45572040d70249408bc8e65671a4
Jacob Koziej <jacobkoziej@gmail.com>
2022-04-04 00:40:04 -0400

    Add parser module

--- p2/parser.py
+++ p2/parser.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-# p2.py -- programming assignement #2
-#          parsing
+# parser.py --  Cocke-Kasami-Younger (CKY) parser
 # Copyright (C) 2022  Jacob Koziej <jacobkoziej@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -15,29 +13,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-import argparse
-
-import parser
-
-
-def main():
-    argparser = argparse.ArgumentParser(description='parsing')
-
-    argparser.add_argument(
-        'grammar',
-        help='context free grammar in chomsky normal form',
-        metavar='grammar.cnf',
-    )
-    argparser.add_argument(
-        '-p',
-        '--parse-tree',
-        action='store_true',
-        help='enable textual parse trees',
-    )
-
-    args = argparser.parse_args()
-
-
-if __name__ == '__main__':
-    main()
